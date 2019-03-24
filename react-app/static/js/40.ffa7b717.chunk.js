23a3de6fb107e9a5fdb0dd8e5a506d0d303cca3f
leonzhang <human1108@163.com>
2019-03-24 20:56:55 +0800

    Site updated: 2019-03-24 20:56:54

---
+++ react-app/static/js/40.ffa7b717.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([40],{383:function(s,n,e){var o=e(609);"string"===typeof o&&(o=[[s.i,o,""]]);var a={hmr:!1,transform:void 0};e(367)(o,a);o.locals&&(s.exports=o.locals)},609:function(s,n,e){(s.exports=e(366)(!0)).push([s.i,".sad-man-wrapper{width:100%}","",{version:3,sources:["/Users/leonzhang/Documents/github/typescript/leon-ts-app/src/pages/SadMan/index.less"],names:[],mappings:"AAAA,iBACE,UAAY,CACb",file:"index.less",sourcesContent:[".sad-man-wrapper {\n  width: 100%;\n}\n"],sourceRoot:""}])}});
+//# sourceMappingURL=40.ffa7b717.chunk.js.map
